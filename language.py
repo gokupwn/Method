29afa357bbf248beb500e179f1ba8ea396e563b5
Hassan-Al-Achek <76757267+Hassan-Al-Achek@users.noreply.github.com>
2021-03-17 17:43:35 +0000

    Add files via upload

---
+++ language.py
@@ -0,0 +1,14 @@
+lang = {
+    "chinise": "再见 ;)",
+    "arabic": "وداعا ;)",
+    "english": "Bye!! ;)",
+    "french": "Au revoir ;)",
+    "russian": "до свидания ;)",
+    "german": "Tschüss ;)",
+    "italy": "addio ;)",
+    "tukish": "Hoşçakal ;)",
+}
+
+
+def good_bye(lan):
+    return lang[lan]
